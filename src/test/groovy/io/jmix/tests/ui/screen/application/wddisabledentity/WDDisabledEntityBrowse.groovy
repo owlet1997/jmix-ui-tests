e239c76b759e835931b94467e16ce3ecca5c6856
owlet1997 <alonewithlight@yandex.ru>
2022-03-10 07:36:09 +0400

    add browser and editor screens for entities

---
+++ src/test/groovy/io/jmix/tests/ui/screen/application/wddisabledentity/WDDisabledEntityBrowse.groovy
@@ -0,0 +1,7 @@
+package io.jmix.tests.ui.screen.application.wddisabledentity
+
+import io.jmix.masquerade.base.Composite
+import io.jmix.tests.ui.screen.administration.datatools.traits.TableActionsTrait
+
+class WDDisabledEntityBrowse extends Composite<WDDisabledEntityBrowse> implements TableActionsTrait {
+}
